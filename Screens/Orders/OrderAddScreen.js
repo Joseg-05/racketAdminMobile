c98a6d90baa74ca4cda25c128d07b3b1e4b3f275
Joseg-05 <49496262+Joseg-05@users.noreply.github.com>
2021-05-06 14:27:32 -0700

    AppBarForAllScreens

--- Screens/Orders/OrderAddScreen.js
+++ Screens/Orders/OrderAddScreen.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Constants from "expo-constants";
 import { Appbar } from "react-native-paper";
 import { Feather } from "@expo/vector-icons";
 import { OrderAddForm } from "../../components/OrderAddForm";
 
+const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
 export const OrderAddScreen = (props) => {
     return (
         <View style={styles.container}>
+             <View style={{
+            width: "100%",
+            height: STATUS_BAR_HEIGHT,
+            backgroundColor: "#1e3d58"
+            }}></View>
             <OrderAddForm {...props} />
         </View>
     );
