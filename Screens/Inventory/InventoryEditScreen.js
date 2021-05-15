8872afedd2b6f8caeb0c11098f36f164ab9cdb01
Sam Yi <samuelyi806@gmail.com>
2021-05-15 03:27:01 -0700

    Inventory Validation
    Implemented basic input validation for inventory, cleaned up code, and added a basic home page

--- Screens/Inventory/InventoryEditScreen.js
+++ Screens/Inventory/InventoryEditScreen.js
@@ -2,31 +2,35 @@ import React, { useEffect } from "react";
 import { StyleSheet, Platform, View } from "react-native";
 import Constants from "expo-constants";
 import { InventoryEditForm } from "../../components/Inventory/InventoryEditForm";
+import { StatusBar } from 'expo-status-bar'
+
 
 const STATUS_BAR_HEIGHT =
     Platform.OS === "ios" ? 20 : Constants.statusBarHeight;
+
 
 export const InventoryEditScreen = (props) => {
     useEffect(() => {}, [props]);
     return (
         <View style={styles.container}>
-            <View
-                style={{
-                    width: "100%",
-                    height: STATUS_BAR_HEIGHT + 20,
-                    backgroundColor: "#1e3d58",
-                }}
-            ></View>
+            <View style={styles.statusBar} ></View>
             <InventoryEditForm {...props} />
         </View>
     );
 };
 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight,
         alignItems: "center",
         backgroundColor: "#36454f",
         justifyContent: "center",
+    },
+    statusBar: {
+        width: '100%',
+        height: STATUS_BAR_HEIGHT,
+        backgroundColor: '#1e3d58',
     },
 });
